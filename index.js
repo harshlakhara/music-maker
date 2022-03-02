188c02355b5935101cd131b57e513d063a3f2f21
harshlakhara <68987112+harshlakhara@users.noreply.github.com>
2022-03-02 11:04:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d360",
+        "#60d3d3",
+        "#d360d3"
+    ];
+    // lets get the sounds
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click" , function () {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+            createBubbles(index);
+        });
+    });
+    // create a function that makes bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener(`animationend` ,function(){
+            visual.removeChild(this);
+        })
+    };
+});
